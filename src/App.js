c61180377ae4dadf6bcb15a0534abc7c483aeef8
Raymond LOUA <louaraym@gmail.com>
2020-01-24 23:06:06 +0100

    Create components : Card, GuessCount, HighScoreInput, HallOfFame; create test files : Card.spec.js, App.spec.js; create smapshots : App.spec.js.snap, Card.spec.js.snap and add others libraries.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,109 @@
-
+import React, { Component } from 'react'
 import './App.css'
-import React from 'react';
 import Card from './Card'
 import GuessCount from './GuessCount'
+import shuffle from 'lodash.shuffle'
+import HallOfFame from './HallOfFame'
+import HighScoreInput from './HighScoreInput'
 
-class App extends React.Component {
-  render() {
-    return (
-        <div className="memory">
-          <GuessCount guesses={0} />
-          <Card card="😀" feedback="hidden" />
-          <Card card="🎉" feedback="justMatched" />
-          <Card card="💖" feedback="justMismatched" />
-          <Card card="🎩" feedback="visible" />
-          <Card card="🐶" feedback="hidden" />
-          <Card card="🐱" feedback="justMatched" />
-        </div>
-    )
+const SIDE = 6;
+export const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿';
+const VISUAL_PAUSE_MSECS = 750
+
+class App extends Component {
+
+    state = {
+        cards: this.generateCards(),
+        currentPair: [],
+        guesses: 0,
+        hallOfFame: null,
+        matchedCardIndices: [],
+    }
+
+    generateCards () {
+        const result = [];
+        const size = SIDE * SIDE;
+        const candidates = shuffle(SYMBOLS);
+        while (result.length < size) {
+            const card = candidates.pop();
+            result.push(card, card)
+        }
+        return shuffle(result)
+    }
+
+    handleNewPairClosedBy(index) {
+        const { cards, currentPair, guesses, matchedCardIndices } = this.state
+
+        const newPair = [currentPair[0], index]
+        const newGuesses = guesses + 1
+        const matched = cards[newPair[0]] === cards[newPair[1]]
+        this.setState({ currentPair: newPair, guesses: newGuesses })
+        if (matched) {
+            this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair] })
+        }
+        setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS)
+    }
+
+    // Arrow fx for binding
+    handleCardClick = index => {
+        const { currentPair } = this.state
+
+        if (currentPair.length === 2) {
+            return
+        }
+
+        if (currentPair.length === 0) {
+            this.setState({ currentPair: [index] })
+            return
+        }
+
+        this.handleNewPairClosedBy(index)
+    }
+
+    getFeedbackForCard(index) {
+        const { currentPair, matchedCardIndices } = this.state
+        const indexMatched = matchedCardIndices.includes(index)
+
+        if (currentPair.length < 2) {
+            return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+        }
+
+        if (currentPair.includes(index)) {
+            return indexMatched ? 'justMatched' : 'justMismatched'
+        }
+
+        return indexMatched ? 'visible' : 'hidden'
+    }
+
+    // Arrow fx for binding
+    displayHallOfFame = (hallOfFame) => {
+        this.setState({ hallOfFame })
+    }
+
+    render() {
+        const { cards, guesses, hallOfFame, matchedCardIndices } = this.state
+        const won = matchedCardIndices.length === cards.length
+        return (
+            <div className="memory">
+              <GuessCount guesses={guesses} />
+                {cards.map((card, index) => (
+                    <Card
+                        card={card}
+                        feedback={this.getFeedbackForCard(index)}
+                        index={index}
+                        onClick={this.handleCardClick}
+                        key={index} />
+                ))}
+                {
+                    won &&
+                    (hallOfFame ? (
+                        <HallOfFame entries={hallOfFame} />
+                    ) : (
+                        <HighScoreInput guesses={guesses} onStored={this.displayHallOfFame} />
+                    ))
+                }
+            </div>
+        )
   }
 }
 
